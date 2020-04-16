6fdd241237806ab26b32c2396a025421cdf26c14
Aashar Wahla <aiwahla.official@gmail.com>
2020-04-16 22:25:37 +0500

    Displays the bottom navigation bar with functionality.

--- lib/navigation_screen.dart
+++ lib/navigation_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:navigationbars/widgets/screen_one.dart';
+import 'package:navigationbars/widgets/screen_two.dart';
 
 class NavigationScreen extends StatefulWidget {
   @override
@@ -6,18 +8,40 @@ class NavigationScreen extends StatefulWidget {
 }
 
 class _NavigationScreenState extends State<NavigationScreen> {
+  // to display the different screen on the different navigation items selected
+  // we will have to write multiple functions.
+  var selectedBarItem = 0;
+  void barTap(index) {   // keep the tapping function with set state out of the
+    setState(() {        // out of the BottomNavigationBar widget
+      selectedBarItem = index;
+    });
+  }
+
+  List<Widget> _screens = [
+    ScreenOne(),
+    ScreenTwo(),
+    Container(
+      child: Text('Profile Selected'),
+    )
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('Navigation Testing App'),),
       ),
+      body: _screens[selectedBarItem],
       bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.teal,
+          unselectedItemColor: Colors.red,
+          currentIndex: selectedBarItem,
           items:  [
             BottomNavigationBarItem(icon: Icon(Icons.phone_iphone), title: Text('Mobile'),),
             BottomNavigationBarItem(icon: Icon(Icons.notifications_none), title: Text('Notifications'),),
             BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text('Profile')),
-          ]
+          ],
+          onTap: barTap,
       ),
     );
   }
